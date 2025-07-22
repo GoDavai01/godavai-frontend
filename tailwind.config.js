bed3bab467466d11244ccac39de9e0b63a09c716
GoDavai01 <info@godavaii.com>
2025-07-23 00:33:27 +0530

    fix it

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // all your app files using Tailwind
+    // Add more paths if you use Tailwind elsewhere
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
