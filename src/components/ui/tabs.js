6632b4ce58adcd695a2488e1a0297a655c87fdc2
GoDavai01 <info@godavaii.com>
2025-08-26 00:13:56 +0530

    fix it

--- src/components/ui/tabs.js
+++ src/components/ui/tabs.js
@@ -2,7 +2,7 @@
 
 import * as React from "react";
 import * as TabsPrimitive from "@radix-ui/react-tabs";
-import { cn } from "../../libs/utils"; // keep this path as you created it
+import { cn } from "../../lib/utils"; // keep this path as you created it
 
 export const Tabs = TabsPrimitive.Root;
 
